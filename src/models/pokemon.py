fc308eb0884be9bbd3e1f84e60851d75554b920c
pedros7 <pedrosoragujobs@gmail.com>
2026-04-16 13:33:13 +0200

    PHASE0 Refactored models + compact

--- src/models/pokemon.py
+++ src/models/pokemon.py
@@ -4,3 +4,18 @@ from sqlmodel import Field, SQLModel
 class Pokemon(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
+
+
+class PokemonStats(SQLModel, table=True):
+    pokemon_id: int = Field(foreign_key="pokemon.id", primary_key=True)
+
+    # Typing
+    type1: str
+    type2: str | None
+
+    # Stats
+    hp: int
+    attack: int
+    defense: int
+    speed: int
+    special: int
---
+++ src/models/trainer.py
@@ -0,0 +1,20 @@
+from sqlmodel import Field, SQLModel
+from src.models.battlepokemon import BattlePokemon
+
+
+class Trainer(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+
+
+class TrainerPokemon(SQLModel, table=True):
+    trainer_id: int = Field(foreign_key="trainer.id", primary_key=True)
+    pokemon_id: int = Field(foreign_key="pokemon.id", primary_key=True)
+
+    slot: int
+
+
+class BattleTrainer:
+    def __init__(self, name, team: list[BattlePokemon]):
+        self.name = name
+        self.team = team
